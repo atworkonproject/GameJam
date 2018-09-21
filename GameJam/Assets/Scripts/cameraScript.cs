d1fa7933ab8241fe7ac59f2b8f91bbd278296075
Arputikos <arputikos@protonmail.com>
2018-09-21 22:02:25 +0200

    Simple game loop

---
+++ GameJam/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+    public GameObject mapObject;
+    private Camera camera;
+
+    float defaultSize;
+
+    Vector2 targetPos;
+    float targetSize;//zoom
+
+	// Use this for initialization
+	void Start () {
+        camera = this.GetComponent<Camera>();
+
+        camera.orthographicSize = defaultSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
