69f9ac8797a9e23574d72988d27a138bb4b3f1cc
Adam <atworkonproject@gmail.com>
2018-09-21 22:50:54 +0200

    no message

---
+++ GameJam/Assets/Scripts/SelectedTileController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedTileController : MonoBehaviour {
+	[Header("to link")]
+	public GameObject SelectedTilePrefab;
+	[Header("other")]
+	public GameObject DisplayedSelectedTile;
+	public Camera MainCam;
+	void Start () {
+		DisplayedSelectedTile = null;
+		MainCam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+	}
+	
+	void Update () {
+		
+	}
+
+	public void DisplayTile(Vector2 position)
+	{
+		if (DisplayedSelectedTile == null)
+		{
+			GameObject newTile = Instantiate(SelectedTilePrefab);
+			newTile.transform.position = position;
+			DisplayedSelectedTile = newTile;
+		}else
+		{
+			DisplayedSelectedTile.transform.position = position;
+		}
+	}
+
+	public void MouseLeftDown()
+	{
+		
+		DisplayTile(MainCam.ScreenToWorldPoint(Input.mousePosition));
+	}
+}
